631a96e2200fc4bf9b6985d0e651cfb489d4ae24
ODUNAYOMI@Accolade <kolade4jesus10@gmail.com>
2021-07-08 23:23:39 +0100

    first commit

---
+++ src/main/java/Contact.java
@@ -0,0 +1,45 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Accolde
+ */
+public class Contact {
+    
+    public String name;
+    public String number;
+
+    public Contact(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" + "name=" + name + ", number=" + number + '}';
+    }
+    
+    
+    
+}
